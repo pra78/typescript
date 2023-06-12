6e4e22d8b3e7ce7a5a644b1b4d7e0ed6431a2c4f
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-12 14:18:56 +0300

    06

---
+++ app.js
@@ -0,0 +1,17 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
